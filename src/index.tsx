d3faec58cbfab70382e43538ae03b51aa2483938
Alexander Shamaev <retrosong@yandex.ru>
2024-06-12 17:03:47 +0300

    refactor(index.tsx, app.tsx, articleparamsform.tsx, arrowbutton.tsx): refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import { App } from './components/app/App';
-
 import './styles/index.scss';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
